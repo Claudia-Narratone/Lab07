8d158a6ab1992490f72eae6897f1c618880a6211
Claudia Narratone <cnarratone@alice.it>
2020-05-11 18:21:23 +0200

    Fatto sceneBuilder

--- Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/FXMLController.java
+++ Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/FXMLController.java
@@ -1,23 +1,50 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.poweroutages;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private ChoiceBox<String> choiceBox;
+
+    @FXML
+    private TextField txtYears;
+
+    @FXML
+    private TextField txtHours;
+
+    @FXML
+    private Button btnAnalysis;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doAnalysis(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert choiceBox != null : "fx:id=\"choiceBox\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtYears != null : "fx:id=\"txtYears\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtHours != null : "fx:id=\"txtHours\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnAnalysis != null : "fx:id=\"btnAnalysis\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
+
